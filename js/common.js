2bf85e4884b02a55004716d9ff88bcbd9659f02d
Denis Pismenny <denis.pismenny@gmail.com>
2014-06-02 00:43:14 +0300

    add header

--- js/common.js
+++ js/common.js
@@ -2,4 +2,30 @@ head.ready(function() {
 
 	console.log("These aren't the droids you're looking for!");
 
+	// Cache the Window object
+	$window = $(window);
+
+	$(' section[data-type="background"] ').each(function(){
+		var $bgobj = $(this); // assigning the object
+
+		$(window).scroll(function() {
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+		});
+	});
+
+	$(window).scroll(function() {
+		if( $(window).scrollTop() > 60 ) {
+			$( '.header' ).addClass('is-fixed');
+		}
+		if( $(window).scrollTop() < 60 ) {
+			$( '.header' ).removeClass('is-fixed');
+		}
+	});
+
 });
